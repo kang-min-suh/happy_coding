a9412ace18533cc9a833438b8840d3ab164e630f
oneumjik <myskeet@gmail.com>
2025-12-14 16:19:29 +0900

    인프런 강의
    코딩테스트 강의 코딩식
    실습한 것 (아직 따라갈만 함)

--- day1.py
+++ day1.py
@@ -1,4 +1,15 @@
-x = 5
-while x > 0: 
-    print(x)
-    x -= 2
+student_names = ["영희", "현준", "딩코", "코딩"]
+student_heights = [166, 198, 188, 100]
+
+def get_180_over_indexes(heights): 
+    result = []
+    for i in range(len(heights)): 
+        height = heights[i]
+        if height > 180: 
+            result.append(i)
+    return result 
+
+over_indexes = get_180_over_indexes(student_heights)
+
+for i in over_indexes: 
+    print(student_names[i])
