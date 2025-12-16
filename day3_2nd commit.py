57e4725ca19af58c370ab364a203e229e8aeac73
oneumjik <myskeet@gmail.com>
2025-12-16 16:02:15 +0900

    사욪자정의 함수_합 구해보기
    퍼플렉시티의 도움으로 사용자정의 함수를 만들어보고, 직접 구현도 해봤다.

--- day3_2nd commit.py
+++ day3_2nd commit.py
@@ -1,20 +1,9 @@
-def find_second_max(array): 
-    if array[0] > array[1]: 
-        first = array[0]
-        second = array[1]
-    else: 
-        first = array[1]
-        second = array[0]
-
-    for num in array[2:]: 
-        if num > first:
-            second = first
-            first = num 
-        elif second < num < first: 
-            second = num 
-  
-    return second 
+def sum_list(array): 
+    sum = 0 
+    for num in array: 
+        sum = sum + num
+    return sum  
  
-print("정답 = 5 / 현재 풀이 값 = ", find_second_max([3, 5, 6, 1, 2, 4]))
+print("정답 = 14 / 현재 풀이 값 = ", sum_list([3, 5, 6]))
 
 
