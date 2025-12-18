5b18f93edc19cbe22ef93ea0856af6db04473f13
oneumjik <myskeet@gmail.com>
2025-12-18 21:20:33 +0900

    result를 이용한 for반복문 출력
    result = []으로 해서
    마지막에 print(result)로 하거나,
    또는
    result를 간단한 for 반복문으로 받아서 처리하는 것도 배웠다.
    if x& ==1 (비트연산자)쓰는 방법도 익혔다.

---
+++ day5.py
@@ -0,0 +1,7 @@
+
+nums = [3, 1, 4, 1, 5]
+result = []
+for x in nums: 
+    if x % 2 == 1: 
+        result.append(x**2)
+print(result)
---
+++ day5_result이용한 표현.py
@@ -0,0 +1,4 @@
+nums = [3, 1, 4, 1, 5]
+
+result = [x**2 for x in nums if x%2 == 1]
+print(result)
