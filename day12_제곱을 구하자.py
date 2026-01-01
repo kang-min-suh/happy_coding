ee9aef7e7086b4a256c390cd35c419bfb9afb2bf
oneumjik <myskeet@gmail.com>
2026-01-01 15:17:33 +0900

    Update day12_제곱을 구하자.py
    어렵지 않지만 아직 익숙하지 않은 구문을 연습해보자

--- day12_제곱을 구하자.py
+++ day12_제곱을 구하자.py
@@ -1,9 +1,8 @@
-# 제곱을 구하자
-# 공백으로 구분하여 두 숫자 a와 b가 주어지면, 
-# a와 b승을 프로그램을 작성하세요 
-
-def r_squqre(a, b):
-    result = a**b
-    return result 
-
-print(r_squqre(3, 2))
+try: 
+    height = int(input())
+    if height >= 150: 
+        print('Yes')
+    else: 
+        print('No')
+except: 
+    print('숫자를 입력하세요')
