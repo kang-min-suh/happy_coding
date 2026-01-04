58da703ccec3966baab4f2d0e2d401fc530434b6
oneumjik <myskeet@gmail.com>
2026-01-04 14:06:02 +0900

    Update 260104.py
    input과 int를 같이쓰면, int는 문자형 정수형태만 받으니까, 소수점까지 입력하는 값을 받으려면 float(input)을 써야 하고, 소수점 특정 자리까지 받으려면 float은 생략하고 round()함수만 활용해도 된다.

--- 260104.py
+++ 260104.py
@@ -1,2 +1,12 @@
-name = input("이름을 입력하세요: ")
-print(name.upper())
+# 함수를 명확하게 사용하기 
+import math
+def circle_area(radius): 
+    result = round(radius**2*math.pi,2)
+    # float는 안쓰고 round() 함수만으로 소수점 둘째자리까지 쓰는 거 표현됨. 
+    return result
+
+# 함수밖에서 입력변수 설정 
+radius = float(input("반지름 값을 입력하세요: "))
+# 소수값도 받으려면 int형이 아니라 float형으로 받아야 한다. 
+# int()는 input()나온 문자열 중에서 정수형태만 받을 수 있다!!! 
+print(circle_area(radius))
