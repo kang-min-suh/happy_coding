fecabc0df583715d9e4ecb6595cdf34448ea0d41
oneumjik <myskeet@gmail.com>
2026-01-11 14:41:34 +0900

    투표 결과를 어떻게 출력할 것인가
    집합형으로 받았다가 다시
    딕셔너리로 받아서 어떻게 출력할지에 대해서 새로운 형식을 배웠다.

--- 260111_2.py
+++ 260111_2.py
@@ -1,4 +1,9 @@
-start_num = int(input("원하는 구구단수: "))
-for i in range(1, 10, 1):
-    print( start_num*i, end = ' ')
-    
+data_int = input().split()
+data_set = set(data_int)
+# data_set
+data_dict = {}
+for key in data_set: 
+    data_dict[key] = data_int.count(key)
+
+max(data_dict, key = data_dict.get)
+print(f'{max(data_dict, key = data_dict.get)}(이)가 총 {max(data_dict.values())}표로 반장이 되었습니다.')
