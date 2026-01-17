1a1aca82caa398d91ec71967b66e251e2d2b2f11
oneumjik <myskeet@gmail.com>
2026-01-17 16:40:07 +0900

    시간 추출위한 정규식 사용방법
    실제 데이터 접목하기 위해 코딩연습

--- 260117.py
+++ 260117.py
@@ -1,7 +1,8 @@
-import retext = "너 스스로 깨닫도록 스스로를 응원하자!  # 12:33  루."
-
-times = re.findall(r'\b\d{2}:\d{2}\b', text)
-print(times)
-
-last_time = times[-1] if times else None
-print(last_time)
+import re 
+text_full = ["나는 행복하다. #23:59 루."]
+pattern = r'\b\d{2}:\d{2}\b'
+last_times = [
+    re.findall(pattern, text)[-1] if re.findall(pattern, text) else None
+    for text in text_full
+]
+print(last_times)
