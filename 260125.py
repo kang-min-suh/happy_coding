4808a73f063625190eb4000a2b2eb17803a688df
oneumjik <myskeet@gmail.com>
2026-01-31 22:20:04 +0900

    숫자를 연속으로 받으려면?
    문자형 자료를 콤마로 받고
    그것을 문자형으로 받는다.

--- 260125.py
+++ 260125.py
@@ -1,16 +1,5 @@
-# 강사님 풀이 (2번째 버전)
-%%timeit
-def check_prime(n): 
-    if n <= 1: 
-        return "NO"
-    i = 2 
-    prime = True
-    while (i**2) < n : 
-        if n % i == 0: 
-            prime = False
-            break
-        i += 1 
-    if prime: 
-        return "YES"
-    else: 
-        return "NO"
+s = ''
+for i in range(1, 21): 
+    s += str(i)
+s
+
