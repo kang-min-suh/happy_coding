f9fb5dfc0df5fd0f2107387598afe60357ff9288
oneumjik <myskeet@gmail.com>
2026-02-14 14:42:44 +0900

    문자열 압축
    for문에서는 튜플형태로 언패킹하여 보관, 만약 a를 aaaa로 반복하면 (a, aaaa)로 하니까, i는 a가 되고, j는 aaaa니까, str(len(j)+1)을 하면 반복숫자가 된다.

--- 260214.py
+++ 260214.py
@@ -1,9 +1,10 @@
-import re 
-input_srt = input()
+import re
 
-range_base = re.findall('(\\w)(\\1*)', input_srt)
+input_str = input()
+
+range_base = re.findall('(\\w)(\\1*)', input_str)
 
 s = ''
 for i, j in range_base: 
-    s += i + str(len(j) + 1)
+    s += i + str(len(j)+1)
 print(s)
