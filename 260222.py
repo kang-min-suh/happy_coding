1e90eb4fb07590349370580a0c96a140a14781a0
oneumjik <myskeet@gmail.com>
2026-02-22 10:35:41 +0900

    알고리즘2
    복습3
    마스터

--- 260222.py
+++ 260222.py
@@ -1,26 +1,24 @@
-# block_all, block_part, rule, char, dummy_value, flag_value 
+# block_all, block_part, rule, char, dummy_value, flag_value
 
 
-def solution(block_all, rule): 
-    for block_part in block_all: 
-        if rule_check(block_part, rule) == '불가능': 
+def solution(block_all, rule):
+    for block_part in block_all:
+        if rule_check(block_part, rule) == '불가능':
             return '불가능'
     return '가능'
 
-
 def rule_check(block_part, rule): 
     dummy_value = rule.index(rule[0])
-    flag_value = False
-    for char in rule: 
+    flag_value = False 
+    for char in block_part:
         if char in rule: 
-            flag_value = True
+            flag_value = True 
             if dummy_value > rule.index(char): 
                 return '불가능'
-            dummy_value = rule.index(char)
     if not flag_value: 
-        return '불가능'
+        return '불가능' 
     return '가능'
-
+        
 
 block_all = input().split(' ')
 rule = input()
