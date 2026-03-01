145c323fd5da5b34a7c7a78a96f21b1400f884c3
oneumjik <myskeet@gmail.com>
2026-03-01 11:09:47 +0900

    소수 알고리즘
    알고리즘 복습

---
+++ 260301.py
@@ -0,0 +1,15 @@
+def prime_list(n): 
+    sieve = [True]*n
+    m = int((n**0.5)) ############
+    for i in range(2, m+1): 
+        if sieve[i] == True:  #######
+            for j in range(i*i, n, i):
+                sieve[j] = False ############
+    return [i for i in range(2, n) if sieve[i] == True]
+
+prime_set = set(prime_list(100))
+n = int(input())
+
+parti = []
+l = [[i, n-i] for i in range(n // 2 + 1) if i in prime_set and n-i in prime_set]
+print(l)
