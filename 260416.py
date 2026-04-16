7c2cebeb290c02e14768c96e7f516fb9594389a9
oneumjik <myskeet@gmail.com>
2026-04-16 12:50:54 +0900

    닉네임 규칙 알고리즘
    문제 풀이
    if문을 for문으로 바꿔서 풀기

--- 260416.py
+++ 260416.py
@@ -1,74 +1,18 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-def func1(num):
-    if 0 > num:
-        return 0
-    else:
-        return num
-
-def func2(num):
-    if num > 0:
-        return 0
-    else:
-        return num
-
-def func3(station):
-    num = 0
-    for people in station:
-        if people == "Off":
-            num += 1
-    return num
-
-def func4(station):
-    num = 0
-    for people in station:
-        if people == "On":
-            num += 1
-    return num
-
-
-def solution(seat, passengers):
-    num_passenger = 0
-    for station in passengers:
-        num_passenger += func
-4(station)
-
-        num_passenger -= func
-3(station)
-
-    answer = func
-1(seat - num_passenger)
-
+def solution(nickname):
+    answer = ""
+    for letter in nickname:
+        if letter == "l":
+            answer += "I"
+        elif letter == "w":
+            answer += "vv"
+        elif letter == "W":
+            answer += "VV"
+        elif letter == "O":
+            answer += "0"
+        else:
+            answer += letter
+    for i in range(4-len(answer)):
+        answer += "o"
+    if len(answer) > 8:
+        answer = answer[:8]
     return answer
