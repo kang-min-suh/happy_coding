4db822c4e05afce5968b1fbc023b18d7415498e6
oneumjik <myskeet@gmail.com>
2026-04-18 12:44:27 +0900

    산책 코딩문제
    문제 파악중

---
+++ 260418.py
@@ -0,0 +1,17 @@
+def solution(route):
+    east = 0
+    north = 0
+    for i in route:
+        if i == "N":
+            north += 1
+        elif i == "S" :
+            
+
+        elif i == "E" :
+            
+
+        elif i == 
+ :
+            
+
+    return [east, north]
