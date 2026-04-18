815a45ff7bd34a02738e4b982c3e862b989aee61
oneumjik <myskeet@gmail.com>
2026-04-18 12:47:33 +0900

    산책 코테
    성공

--- 260418.py
+++ 260418.py
@@ -1,3 +1,16 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
 def solution(route):
     east = 0
     north = 0
@@ -6,12 +19,16 @@ def solution(route):
             north += 1
         elif i == "S" :
             
+north -= 1
 
         elif i == "E" :
             
+east += 1
 
         elif i == 
+"W"
  :
             
+east -= 1
 
     return [east, north]
