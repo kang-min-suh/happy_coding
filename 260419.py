17e5c1a37352f33d8d054791c354fbc25d7b3942
oneumjik <myskeet@gmail.com>
2026-04-19 11:02:36 +0900

    가습기 문제
    준비

---
+++ 260419.py
@@ -0,0 +1,40 @@
+def func1(humidity, val_set):
+    if humidity < val_set:
+        return 
+
+    return 1
+
+def func2(humidity):
+    if humidity >= 50:
+        return 0
+    elif humidity >= 40:
+        return 1
+    elif humidity >= 30:
+        return 2
+    elif humidity >= 20:
+        return 3
+    elif humidity >= 10:
+        return 4
+    
+
+        
+
+
+def func3(humidity, val_set):
+    if humidity < val_set:
+        return 1
+    return 
+
+
+def solution(mode_type, humidity, val_set):
+    answer = 0
+    if mode_type == "auto":
+        answer = func
+
+    elif mode_type == "target":
+        answer = func
+
+    elif mode_type == "minimum":
+        answer = func
+
+    return answer
