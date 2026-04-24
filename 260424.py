f1a2fc77a03a9fe271e0ce4b89264b98e0f2afc6
oneumjik <myskeet@gmail.com>
2026-04-24 10:49:37 +0900

    정처기 실기 복원문제
    문제 이해

---
+++ 260424.py
@@ -0,0 +1,8 @@
+def f(a): 
+    m = [[x] for x in a]
+    b = m[:]
+    for i in rang(len(b)-1): 
+        b[1+i] += b[i]
+    result = sum([len(x) for x in m])
+    return result 
+print(f([1, 2, 3, 4]))
