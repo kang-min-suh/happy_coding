1746e333297bd4e3d4e5914aae464ff1a1d20abf
oneumjik <myskeet@gmail.com>
2026-04-25 10:42:35 +0900

    정처기 파이썬 문제
    재도전

--- 260425.py
+++ 260425.py
@@ -1,21 +1,8 @@
-class Calc {
-    int cc(int a, int b) {
-        return a + b; 
-    }
-    int cc(char a, char b) {
-        return a -b; 
-    }
-    int cc(char c) {
-        return c; 
-    }
-}
-
-public class Test {
-    public static void main(String[] args) {
-        Calc calc = new Calc(); 
-        int r1 = calc.cc(5, 4); 
-        int r2 = calc.cc('c','a'); 
-        char r3 = (char) cal.cc('3'); 
-        System.out.println(r1 + r2 + "2" + r3); 
-    }
-}
+def f(a): 
+    m = [[x] for x in a]
+    b = m[:]
+    for i in range(len(b)-1): 
+        b[1+i] += b[i]
+    result = sum([[len(x) for x in m]])
+    return result 
+print(f([1, 2, 3, 4]))
