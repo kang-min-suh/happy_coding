29258fa74440787f426405ee11c828bfef5ea272
oneumjik <myskeet@gmail.com>
2026-04-26 11:19:59 +0900

    정처기 C코딩 문제
    문제 이해중

---
+++ 260426.py
@@ -0,0 +1,19 @@
+double arr(int *p, int len) {
+    double av = .0; 
+    for(int i = 0; i<len-1; i++) {
+        av += (double)p[i]; 
+    }
+    return av/len; 
+}
+double arr(int *p, int len) {
+    double av = .0; 
+    for(int i = 0; i< len -1; i++) 
+}
+    return av/len; 
+}
+
+int main() {
+    int arr[] = {70, 85, 90, 15, 45, 55, 65, 95, 10, 40}; 
+    int len = 10; 
+    printf("%.2f\n", arr(arr, len) + arrv(arr, len))
+}
