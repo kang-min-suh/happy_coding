0b0029d312500335ab013e58c06acac8802dada0
oneumjik <myskeet@gmail.com>
2026-04-27 23:17:26 +0900

    버그 체크
    ..

---
+++ 260427.java
@@ -0,0 +1,24 @@
+class A {
+    String f(Object x) {
+        return "1"; 
+    }
+    String g() {
+        return f("a"); 
+    }
+}
+
+class B extends A {
+    String f(Object x) {
+        return "2"; 
+    }
+    String f(String x) {
+        return "3"; 
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new B(); 
+        System.out.println(a, g()); 
+    }
+}
