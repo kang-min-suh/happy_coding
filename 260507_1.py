61d22547faea4c0063a9fd96f6a60f9468dec7be
oneumjik <myskeet@gmail.com>
2026-05-07 23:37:36 +0900

    정처기_필기
    복습용

---
+++ 260507_1.py
@@ -0,0 +1,19 @@
+>> 설계12
+
+* MCI : 채널 통합 / 연계 솔루션 
+* FEP 
+* EAI 
+* CRM : 고객 관계 관리 ex) 맥주와 기저귀 
+** VOC: 고객의 소리 (고객 관리 시스템) 
+****  기능/비기능 인터페이스 
+
+* 송/수신 형식 구성 예시: Header - Body - Footer 
+
+*ESB :“Enterprise Service Bus(엔터프라이즈 서비스 버스)”의 약자로, 여러 시스템·애플리케이션 사이에서 메시지 기반 통합을 담당하는 중앙 미들웨어
+
+*UDDI (Universal Description, Discover and Integration) : 정보를 등록하고 검색하기 위한 저장소로 공개적으로 접근, 검색이 가능한 레지스트리 (공용 등록부) 
+* SOAP(Simple Object Access Protcol) 
+
+* WSDL 2.0 기준으로 기억하기 . 
+
+* port: 가상의 연결단 
